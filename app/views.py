5a57b2a8435ed8ed047ecaef9f2ff5bf6081d06f
Adrien Lucbert <adrien.lucbert@epitech.eu>
2019-04-05 13:37:19 +0200

    project basis: app files and default html template

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from app import app
+from flask import Flask, url_for, render_template
+
+@app.route('/', methods=['GET'])
+@app.route('/index', methods=['GET'])
+def route_home():
+    return render_template('index.html')
+
+@app.route('/user/<username>', methods=['POST'])
+def route_add_user(username):
+    return "User added!\n"
